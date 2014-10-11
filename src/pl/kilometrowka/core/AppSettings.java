3469afe7ad9dcfb1eaed5e572b043352a2f3406c
Mateusz86 <m.drozdz6@gmail.com>
2014-10-11 19:44:18 +0200

    add Trasa Utils get from date

--- src/pl/kilometrowka/core/AppSettings.java
+++ src/pl/kilometrowka/core/AppSettings.java
@@ -7,6 +7,7 @@ import android.content.Context;
 public final class AppSettings {
 	public static final String DB_NAME = "kilometrowka-db";
 	public static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat ("HH:mm:ss dd-MM-yyyy");
+	public static final SimpleDateFormat DATE_YMD_FORMAT = new SimpleDateFormat ("dd-MM-yyyy");
 	
 	private static Context context;
 
